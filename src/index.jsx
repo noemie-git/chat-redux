f296dae881511536f684afb0c02b091ea28e5700
Noemie Brancourt <noemie_brancourt@hotmail.com>
2022-01-24 13:56:15 +0100

    initial state

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 // external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -9,6 +10,14 @@ import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
+// eslint-disable-next-line no-unused-vars
+const initialState = {
+  messageList: [],
+  channelList: ["general", "react", "paris"],
+  currentUser: prompt("Enter your username") || `unnamed user${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general'
+};
+
 const reducers = combineReducers({
   changeMe: (state = null, action) => state
 });
